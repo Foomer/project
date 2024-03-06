0b9b0bb37af1ce6493051b53736edfd340780447
foom1r <foom1rrar@gmail.com>
2024-03-06 18:26:43 +0300

    signals

--- HotelHub/hotels/apps.py
+++ HotelHub/hotels/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class HotelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hotels'
+    
+    def ready(self) -> None:
+        from hotels.signals import create_folio
+        from hotels.signals import add_payment_to_folio
+        from hotels.signals import add_folio_posting_to_folio
+
--- HotelHub/hotels/signals.py
+++ HotelHub/hotels/signals.py
@@ -1,8 +1,13 @@
-from models import Payment,FolioPosting
+from hotels.models import Payment,FolioPosting,Guest,Folio
 
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+
+@receiver(post_save, sender=Guest)
+def create_folio(sender,instance,created,**kwargs):
+    if created:
+        Folio.objects.create(user=instance.user, guest=instance)
 
 @receiver(post_save, sender=Payment)
 def add_payment_to_folio(sender, instance, **kwargs):
